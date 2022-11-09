98e87dda0abdc3658a1272698c0b95f2393ddfc9
Victor Kneider <victorkneider@gmail.com>
2022-11-08 22:56:37 -0400

    feat:add css

--- main.js
+++ main.js
@@ -1,9 +1,84 @@
+
+//Elementos HTML
 var contenedor = document.getElementById("contenedor");
+const insert_empleado = document.querySelector('#insert-empleado');
+const insert_concepto = document.querySelector('#insert-concepto');
+const modal_insert_empleado = document.querySelector('#modal-insert-empleado');
+const modal_insert_concepto = document.querySelector('#modal-insert-concepto');
+const close_insert_empleado = document.querySelector('#close-insert-empleado');
+const close_insert_concepto = document.querySelector('#close-insert-concepto');
+
+
+let select_salario = document.getElementById("select-salario");
+let radioPorcentaje = document.getElementById("radio-porcentaje")
+let radioMonto = document.getElementById("radio-neto")
+
+
+let card1= document.getElementById("card1");
+let card2= document.getElementById("card2");
+
+
+let close_asignar_concepto = document.getElementById('close-asignar-concepto');
+let modal_asignar_concepto = document.getElementById('modal-asignar-concepto')
+let btn_asignar_concepto = document.getElementById('asignar-concepto');
+
+
+
+//Eventos
+insert_empleado.addEventListener("click",()=>{
+  modal_insert_empleado.showModal();
+});
+insert_concepto.addEventListener("click",()=>{
+  modal_insert_concepto.showModal();
+});
+
+close_insert_empleado.addEventListener("click", ()=>{
+  modal_insert_empleado.close();
+});
+
+close_insert_concepto.addEventListener("click", ()=>{
+  modal_insert_concepto.close();
+});
+
+btn_asignar_concepto.addEventListener("click", () => {
+  modal_asignar_concepto.showModal();
+})
+
+close_asignar_concepto.addEventListener("click", ()=>{
+  modal_asignar_concepto.close();
+});
+
+
+
+
+function radioChecks(){
+
+  
+  if(radioMonto.checked){
+    card2.classList.remove("block")
+    card1.classList.remove("background")
+    card1.classList.add("block")
+    card2.classList.add("background")
+  }
+  
+  if(radioPorcentaje.checked){
+    card1.classList.remove("block")
+    card2.classList.remove("background")
+    card2.classList.add("block")
+    card1.classList.add("background")
+  }
+
+}
+
+radioMonto.addEventListener('change', radioChecks)
+radioPorcentaje.addEventListener('change', radioChecks)
+
+//JSON
 
 var empleados = [
   {
-    nombre: "armando",
-    apellido: "casas",
+    nombres: "armando",
+    apellidos: "casas",
     urb: "lago country 3",
     postal: "4004",
     cargo: "vendedor",
@@ -11,7 +86,41 @@ var empleados = [
     conceptos: [0, 1, 2],
   },
 
+  {
+    nombres: "francisco",
+    apellidos: "mendoza",
+    urb: "edificios pallium",
+    postal: "4004",
+    cargo: "bobo",
+    cedula: "V30167855",
+    conceptos: [0, 1, 2]
+  }
+
 ];
+
+let formulario = document.getElementById('formulario')
+
+
+let fnsend = function(event){
+
+  let empleado  = {
+    nombre: formulario.Nombres.value,
+    apellido: formulario.Apellidos.value,
+    urb: formulario.Urbanizacion.value,
+    cargo: formulario.cargo-empleado.value,
+    cedula: formulario.Cedula.value,
+  }
+  empleados.push(empleado)
+  alert("El empleado ha sido agregado exitosamente")
+  console.log(empleados)
+  event.preventDefault()
+}
+formulario.addEventListener("submit", fnsend)
+
+
+
+
+
 
 
 var salarios = {
@@ -19,9 +128,26 @@ var salarios = {
   vendedor: 100,
   manager: 150,
   propietario: 200,
-  obrero: 50
-
+  obrero: 50,
+  pasante:0
 }
+
+let textCargo = ""
+let textSalario =""
+let selectCargo = document.getElementById("cargo-empleado")
+
+for (const salario in salarios) {
+  textCargo += `<option value="${salario}"> ${salario} </option>`
+}
+
+for (const salario in salarios) {
+  textSalario += `<option value="${salario}"> ${salario}:${salarios[salario]} </option>`
+}
+
+selectCargo.innerHTML = textCargo;
+select_salario.innerHTML = textSalario;
+
+
 
 
 var conceptos = [
@@ -111,7 +237,7 @@ function generarTabla() {
   
   try {
 
-    var keys = Object.keys(empleados[1]);
+    var keys = Object.keys(empleados[0]);
     var filas = empleados.length;
     var columnas = keys.length;
     contenedor.innerHTML = "";
@@ -169,5 +295,5 @@ function asignarConcepto(cedula, concepto) {
 
   return empleados[findEmpleado(cedula)].conceptos.push(concepto);
 
-
 }
+
